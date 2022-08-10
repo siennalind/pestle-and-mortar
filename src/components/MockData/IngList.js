2dcb1224cd73438e4d056392b980664a98b3120b
siennalind <sienna.lebeaulind@gmail.com>
2022-08-10 11:01:15 +0100

    adding components

---
+++ src/components/MockData/IngList.js
@@ -0,0 +1,42 @@
+const IngList = [
+    {
+        name: 'abecean longfin',
+        effects: ['weakness to frost', 'fortify sneak', 'weakness to poison', 'fortify restoration']
+    },
+    {
+        name: 'blisterwort',
+        effects: ['damage stamina', 'frenzy', 'restore health', 'fortify smithing']
+    },
+    {
+        name: 'creep cluster',
+        effects: ['restore magicka', 'damage stamina regen', 'fortify carry weight', 'weakness to magic']
+    },
+    {
+        name: 'deathbell',
+        effects: ['damage health', 'ravage stamina', 'slow', 'weakness to poison']
+    },
+    {
+        name: 'elves ear',
+        effects: ['restore magicka', 'fortify marksman', 'weakness to frost', 'resist fire']
+    },
+    {
+        name: 'grass pod',
+        effects: ['resist poison', 'ravage magicka', 'fortify alteration', 'restore magicka']
+    },
+    {
+        name: 'honeycomb',
+        effects: ['restore stamina', 'fortify block', 'fortify light armor', 'ravage stamina']
+    },
+    {
+        name: 'imp stool',
+        effects: ['damage health', 'lingering damage health', 'paralysis', 'restore health']
+    },
+    {
+        name: 'lavender',
+        effects: ['resist magic', 'fortify stamina', 'ravage magicka', 'fortify conjuration']
+    },
+    {
+        name: 'purple mountain flower',
+        effects: ['restore stamina', 'fortify sneak', 'lingering damage magicka', 'resist frost']
+    },
+]
