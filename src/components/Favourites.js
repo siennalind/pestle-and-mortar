d5a698bfb56574947d7d4c9bbb8b7d48fd460a4c
siennalind <sienna.lebeaulind@gmail.com>
2022-09-13 16:39:09 +0100

    added favourites component

---
+++ src/components/Favourites.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Favourites () {
+    const [favourites, setFavourites] = useState([])
+
+    const handleFavourite = () => {}
+
+    return (
+        {favourites}
+    )
+}
+
+export default Favourites
