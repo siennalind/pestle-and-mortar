188d27aba3fa2ce83d47b1fcc082dfbea8269d82
siennalind <sienna.lebeaulind@gmail.com>
2022-09-25 18:38:51 +0100

    ui elements

---
+++ src/components/Layout/Nav.js
@@ -0,0 +1,12 @@
+function Nav () {
+    return (
+        <Nav>
+            <ul>
+                <li>Search</li>
+                <li>Favourites</li>
+            </ul>
+        </Nav>
+    )
+}
+
+export default Nav
