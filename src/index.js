aa966b4920df60e93603dc1e94765d94d1d89b78
siennalind <sienna.lebeaulind@gmail.com>
2023-03-27 16:30:29 +0100

    imported fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "@fontsource/gabriela"
+import "@fontsource/imprima"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
